7c596a54379b3f6d8c24c79825d710f15fd44211
Marie Backman <marie.backman@gmail.com>
2017-12-04 14:54:53 +0100

    backend initial objects and the first unit test

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,2 +1,9 @@
 var a = 5;
+angular.module('demo', [])
+    .controller('Hello', function ($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        then(function (response) {
+        $scope.greeting = response.data;
+    });
+});
 //# sourceMappingURL=app.js.map
---
+++ src/test/java/com/weatheryoulikeit/application/WebControllerTests.java
@@ -0,0 +1,28 @@
+package com.weatheryoulikeit.application;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class WebControllerTests {
+
+    private WebController sut;
+
+    @Before
+    public void setup() {
+        sut = new WebController();
+    }
+
+    @Test
+    public void getFlightResults() {
+        FlightData flightData = new FlightData();
+        assertEquals(flightData, sut.getFlightResults("ARN", "2018-01-01", "2018-01-05", 20, 30));
+    }
+
+}
