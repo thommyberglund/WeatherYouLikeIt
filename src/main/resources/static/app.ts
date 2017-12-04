33d91453e60a62c05c340885e88bf672249e7a3a
anno0406 <angelica.nordqvist@gmail.com>
2017-12-04 21:59:21 +0100

    added angular function that takes data from the form, sends it and returns new data in divs.

--- src/main/resources/static/app.ts
+++ src/main/resources/static/app.ts
@@ -1,9 +1,41 @@
-var a: number = 5;
+let module = angular.module('demo', ['ngSanitize']);
 
-angular.module('demo', [])
-    .controller('Hello', function($scope, $http) {
-        $http.get('http://rest-service.guides.spring.io/greeting').
-        then(function(response) {
-            $scope.greeting = response.data;
-        });
+
+module.controller('Hello', function ($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
+        $scope.greeting = response.data;
     });
+});
+
+
+module.controller('search', function ($scope, $http) {
+    $scope.sendToBackEnd = () => {
+        console.log(JSON.stringify($scope.data));
+
+//        $http.post('http://rest-service.guides.spring.io/greeting', $scope.data).then(function (response) {
+        $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
+
+            //let data = response.data;
+
+            let data = [
+                {country: "Muffinland", degrees: 50, price: 10},
+                {country: "Kaninland", degrees: 50, price: 10},
+                {country: "Minland", degrees: 50, price: 10}
+            ];
+
+            let htmlResult = "";
+            data.forEach((d) => {
+                htmlResult +=
+                    '<div class="resultDiv">' +
+                    '<span>' + d.country + '</span>' +
+                    '<span>' + d.degrees + '</span>' +
+                    '<span>' + d.price + '</span>' +
+                    '</div>';
+            });
+
+            $scope.myHTML = htmlResult;
+
+        });
+
+    };
+});
---
+++ src/main/resources/static/data.ts
@@ -0,0 +1,7 @@
+import {Destination} from "./interface";
+
+export const cityData: Destination = {
+    city: ["New York", "Paris", "Hudson"],
+    price: ["2300", "2100", "2400"],
+    temperature: ["21", "24", "27"]
+};
