0451f35e5a010f93346c9844621e33439269f7d8
Jesper <jetolu@gmail.com>
2017-12-05 14:26:15 +0100

    Add results layout, resize logo etc.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,16 +1,25 @@
-var module = angular.module('demo', ['ngSanitize']);
+var angmodule = angular.module('demo', ['ngSanitize']);
 /*module.controller('Hello', function ($scope, $http) {
     $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
         $scope.greeting = response.data;
     });
 });*/
-module.controller('search', function ($scope, $http) {
+angmodule.controller('search', function ($scope, $http) {
     $scope.sendToBackEnd = function () {
         console.log(JSON.stringify($scope.data));
         //        $http.post('http://rest-service.guides.spring.io/greeting', $scope.data).then(function (response) {
         $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
             //let data = response.data;
             var data = [
+                { country: "Muffinland", degrees: 21, price: 1025 },
+                { country: "Kaninland", degrees: 27, price: 2750 },
+                { country: "Minland", degrees: 56, price: 2687 },
+                { country: "Muffinland", degrees: 21, price: 1025 },
+                { country: "Kaninland", degrees: 27, price: 2750 },
+                { country: "Minland", degrees: 56, price: 2687 },
+                { country: "Muffinland", degrees: 21, price: 1025 },
+                { country: "Kaninland", degrees: 27, price: 2750 },
+                { country: "Minland", degrees: 56, price: 2687 },
                 { country: "Muffinland", degrees: 21, price: 1025 },
                 { country: "Kaninland", degrees: 27, price: 2750 },
                 { country: "Minland", degrees: 56, price: 2687 },
@@ -31,7 +40,11 @@ module.controller('search', function ($scope, $http) {
                         '<span>' + d.price + '</span>' +
                         '</div>';
             });
-            $scope.myHTML = htmlResult;
+            var result = '<h1>We found ' + data.length + ' trips!</h1>' + '<h6>' +
+                '<span>Destination</span>' +
+                '<span>Avgerage temperature</span>' +
+                '<span>Price</span>' + '</h6>';
+            $scope.myHTML = result + htmlResult;
         });
     };
 });
