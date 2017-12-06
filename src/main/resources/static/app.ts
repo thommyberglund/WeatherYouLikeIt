8c721575ba928c48ff21c6e507d7d7e33ef30860
Jesper <jetolu@gmail.com>
2017-12-06 09:36:30 +0100

    Fixed errors from last push

--- src/main/resources/static/app.ts
+++ src/main/resources/static/app.ts
@@ -1,39 +1,43 @@
 var angmodule = angular.module('demo', ['ngSanitize']);
 
 
+/*module.controller('Hello', function ($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
+        $scope.greeting = response.data;
+    });
+});*/
+
+
 angmodule.controller('search', function ($scope, $http) {
     $scope.sendToBackEnd = () => {
-
-        function splitTheString(date) {
-            var dateFormat = (JSON.stringify(date));
-            var noSnuffs = dateFormat.replace('"', '');
-            var splitDate = noSnuffs.split('T');
-            date = splitDate[0];
-            return date;
-        }
-
-        $scope.data.startDate = splitTheString($scope.data.startDate);
-        $scope.data.endDate = splitTheString($scope.data.endDate);
-
         console.log(JSON.stringify($scope.data));
 
-        $http.post('https://weatheryoulikeit.herokuapp.com/getFlightResults', $scope.data).then(function (response) {
-            //$http.get('http://rest-service.guides.spring.io/greeting' + $scope.data).then(function (response) {
+//        $http.post('http://rest-service.guides.spring.io/greeting', $scope.data).then(function (response) {
+        $http.get('http://rest-service.guides.spring.io/greeting').then(function (response) {
 
-            let data = response.data;
+            //let data = response.data;
 
-            // let data = [
-            //     {country: "Muffinland", degrees: 21, price: 1025},
-            //     {country: "Kaninland", degrees: 27, price: 2750},
-            //     {country: "Minland", degrees: 56, price: 2687},
-            //     {country: "Muffinland", degrees: 21, price: 1025},
-            //     {country: "Kaninland", degrees: 27, price: 2750},
-            //     {country: "Minland", degrees: 56, price: 2687},
-            //     {country: "Muffinland", degrees: 21, price: 1025},
-            //     {country: "Kaninland", degrees: 27, price: 2750},
-            //     {country: "Minland", degrees: 56, price: 2687},
-            //     {country: "Minland", degrees: 50, price: 1560}
-            // ];
+            let data = [
+                {country: "Muffinland", degrees: 21, price: 1025},
+                {country: "Kaninland", degrees: 27, price: 2750},
+                {country: "Minland", degrees: 56, price: 2687},
+                {country: "Muffinland", degrees: 21, price: 1025},
+                {country: "Kaninland", degrees: 27, price: 2750},
+                {country: "Minland", degrees: 56, price: 2687},
+                {country: "Muffinland", degrees: 21, price: 1025},
+                {country: "Kaninland", degrees: 27, price: 2750},
+                {country: "Minland", degrees: 56, price: 2687},
+                {country: "Muffinland", degrees: 21, price: 1025},
+                {country: "Kaninland", degrees: 27, price: 2750},
+                {country: "Minland", degrees: 56, price: 2687},
+                {country: "Muffinland", degrees: 21, price: 1025},
+                {country: "Kaninland", degrees: 27, price: 2750},
+                {country: "Minland", degrees: 56, price: 2687},
+                {country: "Muffinland", degrees: 21, price: 1025},
+                {country: "Kaninland", degrees: 27, price: 2750},
+                {country: "Minland", degrees: 56, price: 2687},
+                {country: "Minland", degrees: 50, price: 1560}
+            ];
 
             let htmlResult = "";
             data.forEach((d) => {
