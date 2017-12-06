0b4017330814dbc8be02e5f6633facd4a3baf3b6
Jesper <jetolu@gmail.com>
2017-12-06 07:43:13 +0100

    Add styling to range slider, change logo size, add our idea text, change font

--- src/main/resources/static/app.ts
+++ src/main/resources/static/app.ts
@@ -1,13 +1,12 @@
 var angmodule = angular.module('demo', ['ngSanitize']);
 
 
-
 angmodule.controller('search', function ($scope, $http) {
     $scope.sendToBackEnd = () => {
 
         function splitTheString(date) {
             var dateFormat = (JSON.stringify(date));
-            var noSnuffs = dateFormat.replace('"','' );
+            var noSnuffs = dateFormat.replace('"', '');
             var splitDate = noSnuffs.split('T');
             date = splitDate[0];
             return date;
@@ -19,7 +18,7 @@ angmodule.controller('search', function ($scope, $http) {
         console.log(JSON.stringify($scope.data));
 
         $http.post('https://weatheryoulikeit.herokuapp.com/getFlightResults', $scope.data).then(function (response) {
-        //$http.get('http://rest-service.guides.spring.io/greeting' + $scope.data).then(function (response) {
+            //$http.get('http://rest-service.guides.spring.io/greeting' + $scope.data).then(function (response) {
 
             let data = response.data;
 
@@ -46,16 +45,49 @@ angmodule.controller('search', function ($scope, $http) {
                     '</div>';
             });
 
-            let result = '<h1>We found ' + data.length + ' trips!</h1>' + '<h6>' +
+            let result = '<h1>We found ' + data.length + ' trips!</h1>' + '<h3>' +
                 '<span>Destination</span>' +
                 '<span>Average temperature</span>' +
-                '<span>Price</span>' + '</h6>';
+                '<span>Price</span>' + '</h3>';
 
             $scope.myHTML = result + htmlResult;
-            
+
+
         });
 
     };
 
 
 });
+
+function getVals() {
+    // Get slider values
+    var parent = this.parentNode;
+    var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat(slides[0].value);
+    var slide2 = parseFloat(slides[1].value);
+    // Neither slider will clip the other, so make sure we determine which is larger
+    if (slide1 > slide2) {
+        var tmp = slide2;
+        slide2 = slide1;
+        slide1 = tmp;
+    }
+
+    var displayElement = parent.getElementsByClassName("rangeValues")[0];
+    displayElement.innerHTML = slide1 + " - " + slide2;
+}
+
+window.onload = function () {
+    // Initialize Sliders
+    var sliderSections = document.getElementsByClassName("range-slider");
+    for (var x = 0; x < sliderSections.length; x++) {
+        var sliders = sliderSections[x].getElementsByTagName("input");
+        for (var y = 0; y < sliders.length; y++) {
+            if (sliders[y].type === "range") {
+                sliders[y].oninput = getVals;
+                // Manually trigger event first time to display values
+                sliders[y].oninput();
+            }
+        }
+    }
+};
